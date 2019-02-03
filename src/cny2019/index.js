525b1c71e88009ad9b5c7d414f3cd952c8a00465
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2019-02-03 18:31:10 +0000

    Horizontal screen movement and object spawns working

--- src/cny2019/index.js
+++ src/cny2019/index.js
@@ -25,7 +25,7 @@ export class CNY2019 extends Story {
     
     //Config
     //--------------------------------
-    avo.config.debugMode = false;
+    avo.config.debugMode = true;
     //--------------------------------
         
     //Images
@@ -34,14 +34,7 @@ export class CNY2019 extends Story {
     avo.assets.images.fireworks = new ImageAsset("assets/cny2019/fireworks.png");
     avo.assets.images.background = new ImageAsset("assets/cny2019/city-background.png");
     avo.assets.images.comicIntro1 = new ImageAsset("assets/cny2019/comic-intro-1.png");
-    avo.assets.images.comicIntro2 = new ImageAsset("assets/cny2019/comic-intro-2.png");
-    avo.assets.images.comicIntro3 = new ImageAsset("assets/cny2019/comic-intro-3.png");
-    avo.assets.images.comicIntro4 = new ImageAsset("assets/cny2019/comic-intro-4.png");
-    avo.assets.images.comicIntro5 = new ImageAsset("assets/cny2019/comic-intro-5.png");
     avo.assets.images.comicWin1 = new ImageAsset("assets/cny2019/comic-win-1.png");
-    avo.assets.images.comicWin2 = new ImageAsset("assets/cny2019/comic-win-2.png");
-    avo.assets.images.comicWin3 = new ImageAsset("assets/cny2019/comic-win-3.png");
-    avo.assets.images.comicLose = new ImageAsset("assets/cny2019/comic-lose.png");
     //--------------------------------
 
     //Animations
@@ -127,23 +120,7 @@ export class CNY2019 extends Story {
     const avo = this.avo;
     
     if (avo.state === AVO.STATE_ACTION) {
-      if (avo.playerActor.x < 0) avo.playerActor.x = 0;
-      if (avo.playerActor.y < 0) avo.playerActor.y = 0;
-      if (avo.playerActor.x > avo.canvasWidth) avo.playerActor.x = avo.canvasWidth;
-      if (avo.playerActor.y > avo.canvasHeight) avo.playerActor.y = avo.canvasHeight;
-
-      avo.store.flyingSpeed = Math.floor(
-        (avo.playerActor.x / avo.canvasWidth) * 
-        (avo.store.FLYING_SPEED_MAX - avo.store.FLYING_SPEED_MIN) +
-        avo.store.FLYING_SPEED_MIN
-      );
-      avo.store.time++;
-      avo.store.distance += avo.store.flyingSpeed;
-
-      //Win condition?
-      if (avo.store.distance >= avo.store.TARGET_DISTANCE) {
-        avo.changeState(AVO.STATE_COMIC, playWinComic);
-      }
+      this.runRacingGame();
     }
   }
   
@@ -170,9 +147,10 @@ export class CNY2019 extends Story {
     avo.actors = [];
     avo.store = {
       distance: 0,
-      flyingSpeed: 0,
-      FLYING_SPEED_MIN: 2,
-      FLYING_SPEED_MAX: 16,
+      TARGET_DISTANCE: 1000,
+      runningSpeed: 0,
+      RUNNING_SPEED_MIN: 2,
+      RUNNING_SPEED_MAX: 16,
       tick: 0,
       TICK_MAX: AVO.FRAMES_PER_SECOND * 2,
       time: 0,
@@ -189,7 +167,79 @@ export class CNY2019 extends Story {
     avo.playerActor.animationSet = avo.animationSets.rooster;
     avo.playerActor.attributes[AVO.ATTR.SPEED] = 8;
     avo.playerActor.rotation = AVO.ROTATION_EAST;
+    avo.playerActor.shadowSize = 0;
     avo.actors.push(avo.playerActor);
     //--------------------------------
+  }
+  
+  runRacingGame() {
+    const avo = this.avo;
+    
+    //Player: keep in bounds of screen
+    //--------------------------------
+    if (avo.playerActor.x < 0) avo.playerActor.x = 0;
+    if (avo.playerActor.y < 0) avo.playerActor.y = 0;
+    if (avo.playerActor.x > avo.canvasWidth) avo.playerActor.x = avo.canvasWidth;
+    if (avo.playerActor.y > avo.canvasHeight) avo.playerActor.y = avo.canvasHeight;
+    //--------------------------------
+
+    //Time and Space
+    //--------------------------------
+    avo.store.runningSpeed = Math.floor(
+      (avo.playerActor.x / avo.canvasWidth) * 
+      (avo.store.RUNNING_SPEED_MAX - avo.store.RUNNING_SPEED_MIN) +
+      avo.store.RUNNING_SPEED_MIN
+    );
+    avo.store.time++;
+    avo.store.distance += avo.store.runningSpeed;
+    //--------------------------------
+
+    //Win condition?
+    //--------------------------------
+    //if (avo.store.distance >= avo.store.TARGET_DISTANCE) {
+    //  avo.changeState(AVO.STATE_COMIC, this.playWinComic);
+    //}
+    //--------------------------------
+    
+    //Run physics for non-player Actors.
+    //--------------------------------
+    avo.actors.map((actor) => {
+      if (actor === avo.playerActor) return;
+
+      //Everything scrolls past!
+      actor.x -= avo.store.runningSpeed;
+
+      //Check if something is colliding with the player.
+      if (Physics.checkCollision(actor, avo.playerActor)) {
+        
+      }
+    });
+
+    //Add new stuff.
+    this.spawnRandomObject(avo.store.distance / avo.store.TARGET_DISTANCE * 100);
+
+    //Clean up! If it's not the player or on the screen, get rid of it.
+    avo.actors = avo.actors.filter((actor) => {
+      return actor === avo.playerActor || actor.x >= 0;
+    });
+  }
+  
+  spawnRandomObject(distancePercent = 100) {
+    const avo = this.avo;    
+    if (Math.random() > 0.05) return;
+    
+    console.log('+++', avo.actors.length);
+
+    const x = avo.canvasWidth * 1.1;
+    const y = 50;
+    const actor = new Actor("coin", x, y, 32, AVO.SHAPE_CIRCLE);
+    actor.solid = false;
+    actor.spritesheet = avo.assets.images.fireworks;
+    actor.animationSet = avo.animationSets.fireworks;
+    actor.playAnimation("idle");
+    actor.rotation = AVO.ROTATION_NORTH;
+    //actor.attributes[AVO.ATTR.SPEED] = Math.floor(Math.random() * 8 + 4);
+    avo.actors.push(actor);
+    console.log('+++', avo.actors.length);
   }
 }
